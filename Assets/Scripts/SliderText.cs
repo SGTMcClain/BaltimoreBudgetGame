d67f4a59cc820aee4743cd6f08e9d44d6bb342da
SGTMcClain <n8mcclain@gmail.com>
2015-06-16 04:05:21 -0400

    Added City Hall
    Added CityHall model

---
+++ Assets/Scripts/SliderText.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderText : MonoBehaviour {
+
+	public GameObject GUITextObject;
+	Text txt;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+
+	public void SetText (Text newText){
+		txt = newText;
+	}
+}
