3ee04f2756031ff6dfd3dc3da1fe084279a7b2c1
SGTMcClain <n8mcclain@gmail.com>
2015-06-28 22:25:35 -0400

    Scenario Buttons Working
    Finally got the scenario buttons working properly.  They can reduce or
    raise the amount of money in the budget.  To change the value look at
    the inspector for the button.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static float budgetTotal = 1000f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	}
+
+	public void BudgetAction(float changeBudgetAmount){
+
+		budgetTotal += changeBudgetAmount;
+		Debug.Log ("Button Clicked");
+
+	}
+}
